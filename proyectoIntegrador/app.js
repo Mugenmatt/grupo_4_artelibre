3290d70b1761acef4d65f68a3d490278a79adcaa
juangc98 <juangc_98@hotmail.com>
2020-05-23 17:02:43 -0300

    Ajustes en header + app.js y bin/www

--- proyectoIntegrador/app.js
+++ proyectoIntegrador/app.js
@@ -1,3 +1,4 @@
+// ************ Require's ************
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -41,10 +42,13 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+
+// ************ DON'T TOUCH FROM HERE ************
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
+  res.locals.path = req.path;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
