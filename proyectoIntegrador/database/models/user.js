993e25d088df867f86d68480127af06e93817606
juangc98 <juangc_98@hotmail.com>
2020-06-22 11:38:25 -0300

    actualizacion DB + comentarios en database/models/

--- proyectoIntegrador/database/models/user.js
+++ proyectoIntegrador/database/models/user.js
@@ -2,14 +2,14 @@
 module.exports = (sequelize, DataTypes) => {
   
   const User = sequelize.define('User', {
-    name: DataTypes.STRING,
+    name: DataTypes.STRING,     //JUAN: No falta id aca??
     lastname:DataTypes.STRING,
     username: DataTypes.STRING,
     email: DataTypes.STRING,
     avatar: DataTypes.STRING,
     password: DataTypes.STRING,
-    rol: DataTypes.BOOLEAN, // FACU: esto deberia ser tinyint(4), talvez no es boolean
-    mailShipping: DataTypes.BOOLEAN,
+    rol: DataTypes.BOOLEAN, // FACU: esto deberia ser tinyint(4), talvez no es boolean --- JUAN: Exacto. Boolean trae problemas, se resuelve con la misma logica pero con el Type Tinyint.
+    mailShipping: DataTypes.BOOLEAN,     // JUAN: Lo mismo aca. se aplica misma logica de boolean pero con tinyint.
     privateShipping: DataTypes.BOOLEAN,
     noShipping: DataTypes.BOOLEAN,
     averageScore: DataTypes.DECIMAL,
